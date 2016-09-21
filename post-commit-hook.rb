7efa80775bdb6f7244a0442e2ce411f6cd59d1f1
jimdoescode <jimdoescode@gmail.com>
2016-09-21 14:22:00 -0400

    Removed logger and it's working

--- post-commit-hook.rb
+++ post-commit-hook.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 
 require 'git'
+require 'logger'
 
-g = Git.open('/Users/jim/Programs/jekyll-commit-blog', :log => Logger.new(STDOUT))
+puts 'TEST'
+g = Git.open('/Users/jim/Programs/jekyll-commit-blog')
 g.log.each { |l| puts l.author.name }
