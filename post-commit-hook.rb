90d930691b594e2ebd8f6c25797e3e5511ac5dd9
jimdoescode <jimdoescode@gmail.com>
2016-09-21 19:09:41 -0400

    {BLOG tags=[ruby,git]&layout=test} Testing passing parameters for the front matter

--- post-commit-hook.rb
+++ post-commit-hook.rb
@@ -3,14 +3,13 @@
 require 'git'
 
 git = Git.open('/Users/jim/Programs/jekyll-commit-blog')
-raw = git.log[0].message.match(/^\[BLOG(.*)\](.*)$/im)
-author = git.log[0].author.name
-datetime = git.log[0].date.strftime("%Y-%m-%d %H:%M:%S")
+raw = git.log[0].message.match(/^\{BLOG(.*)\}(.*)$/im)
 
 # If they didn't use the BLOG 
 # keyword then nothing to do 
 exit 0 if raw.nil? 
 
+params = raw[1].empty? {} : CGI::parse(raw[1].strip)
 # Set the blog post body
 body = raw[2].strip
 
@@ -21,13 +20,19 @@ parts = body.match(/^(.*)(?:\r|\n|\r\n){2}(.*)$/m);
 title = (parts.nil? || parts.length < 3) ? body[0...60].strip : parts[1].strip 
 slug = git.log[0].date.strftime("%Y-%m-%d") + '-' + title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
 
+frontmatter = {
+    "layout" => "post",
+    "author" => git.log[0].author.name,
+    "title" => "\"#{title}\"",
+    "date" => git.log[0].date.strftime("%Y-%m-%d %H:%M:%S"),
+}.merge(params)
+
 post = File.open(slug + '.md', 'w+')
 # Add the front matter
 post.puts('---')
-post.puts('layout: post')
-post.puts('author: ' + author)
-post.puts('title:  "' + title + '"')
-post.puts('date:   ' + datetime)
+frontmatter.each { |key, value|
+    puts "#{key}: #{value}"
+}
 post.puts('---')
 post.puts('')
 post.puts(body)
