42fc78c957989590e380723a03c0339f7ebdcbb4
jimdoescode <jimdoescode@gmail.com>
2016-09-21 18:27:59 -0400

    [BLOG tags='ruby,git,jekyll'] Adding extra parameters to the blog tag

--- post-commit-hook.rb
+++ post-commit-hook.rb
@@ -3,7 +3,7 @@
 require 'git'
 
 git = Git.open('/Users/jim/Programs/jekyll-commit-blog')
-raw = git.log[0].message.match(/^\[BLOG\](.*)/m)
+raw = git.log[0].message.match(/^\[BLOG(.*)\](.*)$/im)
 author = git.log[0].author.name
 datetime = git.log[0].date.strftime("%Y-%m-%d %H:%M:%S")
 
@@ -12,16 +12,17 @@ datetime = git.log[0].date.strftime("%Y-%m-%d %H:%M:%S")
 exit 0 if raw.nil? 
 
 # Set the blog post body
-body = raw[1].strip
+body = raw[2].strip
 
 # Attempt to separate a title from the body
-parts = body.match(/^(.*)(?:\r|\n|\r\n){2}(.*)$/);
+parts = body.match(/^(.*)(?:\r|\n|\r\n){2}(.*)$/m);
 
 # If we can't separate a title from the body then just use the first 60 chars of the body
 title = (parts.nil? || parts.length < 3) ? body[0...60].strip : parts[1].strip 
 slug = git.log[0].date.strftime("%Y-%m-%d") + '-' + title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
 
 post = File.open(slug + '.md', 'w+')
+# Add the front matter
 post.puts('---')
 post.puts('layout: post')
 post.puts('author: ' + author)
